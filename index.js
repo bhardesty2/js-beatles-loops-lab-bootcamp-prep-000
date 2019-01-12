8064ac3d140ca731d702a0320ed3659cb2a618d1
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2019-01-12 03:46:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + ' plays ' + instruments[i]);
   }
-  return arrays;
+  return array;
 }
 
 function johnLennonFacts(facts) {
